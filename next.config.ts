1db204afe7be1556850aaf8d0576c7cc20069dfd
fahimad24 <mdtaraquerahmanfahim@gmail.com>
2026-08-28 00:12:45 +0600

    feat: add security headers to enhance content security policy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            // img-src a 'blob:' add kora hoyeche preview er jonno
+            // frame-src add kora hoyeche youtube video iframe er jonno
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https://lh3.googleusercontent.com https://res.cloudinary.com https://images.unsplash.com; font-src 'self'; connect-src 'self'; frame-src 'self' https://www.youtube.com https://www.youtube-nocookie.com; frame-ancestors 'none';",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          }
+        ],
+      },
+    ];
+  },
   images: {
     qualities: [60, 65, 75],
     remotePatterns: [
@@ -18,7 +41,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  
 };
 
 export default nextConfig;
